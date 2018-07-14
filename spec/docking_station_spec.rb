4545c456bfbe1d7e4f9b46c87d94d7105d129bfb
Marcus <marcus.kerr94@gmail.com>
2018-07-14 23:16:08 +0100

    Unit test: Expect release bike method to produce a bike object (RED)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
 
     # Expect Docking station instance to respond to release_bike method
     it { is_expected.to respond_to :release_bike }
+
+    it "Gets a bike" do
+        expect(subject.release_bike).to be_an_instance_of(Bike)
+    end 
 end

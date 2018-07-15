0e8fc67e992a3913b88a280cf63e0d4edd417077
Marcus <marcus.kerr94@gmail.com>
2018-07-15 14:22:55 +0100

    Feature: default capacity of 20 bikes. Updated unit test (RED)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -32,7 +32,7 @@ describe DockingStation do
 
     describe "#dock" do
         it 'raises an error when the docking station is full' do
-            subject.dock(Bike.new)
+            20.times { subject.dock Bike.new }
             expect{ subject.dock Bike.new }.to raise_error "Docking Station full"
         end
     end

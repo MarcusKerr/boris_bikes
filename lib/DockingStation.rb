db0fa526af4c34870de7f0b4b95ff80a03cfe95d
Marcus <marcus.kerr94@gmail.com>
2018-07-15 15:09:09 +0100

    Refactoring: created private predicate methods

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,18 +1,31 @@
 
 require_relative "Bike"
 class DockingStation
+    attr_reader :bike
     def initialize
         @bikes = []
+    end
+
+    private
+        # Predicate method to check if dock is full
+        def full?
+            @bikes.count >= 20 
+        end 
+        
+        # Private method to check if dock is empty
+        def empty?
+            @bikes.empty?
+        end 
     end
     
     # Releade bike method
     def release_bike
-       fail "No bikes available" if @bikes.empty?
+       fail "No bikes available" if empty?
        @bikes.pop
     end
 
     def dock(bike)
-        fail "Docking Station full" if @bikes.count >= 20
+        fail "Docking Station full" if full?
         @bikes << bike
     end
 end

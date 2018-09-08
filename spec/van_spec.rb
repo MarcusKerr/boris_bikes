1fd38de28b130e7f1802ccae2b74feadde005dcd
Marcus <marcus.kerr94@gmail.com>
2018-09-08 17:46:07 +0100

    Feature: collect fixed bikes distribute, FT - RED, UT - RED

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,7 +3,8 @@ require 'Van'
 describe Van do
   let(:bike) { double :bike_instance}
   let(:docking_station) { double :DockingStation, broken_bikes: [bike] }
-  let(:garage) { double :garage }
+  let(:garage) { double :garage, fixed_bikes: [bike] }
+
 
   describe '#capacity' do
     it 'has a capacity fo 5 bikes' do
@@ -17,6 +18,12 @@ describe Van do
       allow(bike).to receive(:working?).and_return(false)
       subject.collect_bikes(docking_station)
       expect(subject.bikes).to eq(docking_station.broken_bikes)
+    end
+
+    it 'collects fixed bikes from garages' do
+      allow(garage).to receive(:is_a?).and_return(true)
+      subject.collect_bikes(garage)
+      expect(subject.bikes).to eq(garage.fixed_bikes)
     end 
   end
 

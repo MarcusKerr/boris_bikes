8728fa64212ccc5b50948e281b9ae70cfd8be8fc
Marcus <marcus.kerr94@gmail.com>
2018-09-08 17:09:38 +0100

    UT: fix bikes - RED

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -4,10 +4,20 @@ describe Garage do
   let(:bike) { double :bike_instance }
   let(:bike2) { double :bike_instance }
   let(:van) { double :van_instance, bikes: [bike, bike2]}
+  
   describe '#unload' do
     it 'add bikes to the bikes array' do
       subject.unload(van.bikes)
       expect(subject.bikes).to eq([bike, bike2])
     end 
+  end
+
+  describe '#fix_bikes' do
+    it 'fixes broken bikes' do
+      broken_bikes = [bike, bike2]
+      subject.fix_bikes
+      expect(subject.broken_bikes).to be_empty
+      expect(subject.fixed_bikes).to eq([bike, bike2])
+    end
   end
 end
